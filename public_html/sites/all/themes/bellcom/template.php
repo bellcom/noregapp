6feede33e3d2c08c63ecbd709511b902f2894eec
Morten Nissen <mn@bellcom.dk>
2015-03-18 10:49:23 +0100

    Updated

--- public_html/sites/all/themes/bellcom/template.php
+++ public_html/sites/all/themes/bellcom/template.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // Change CSS
 function bellcom_css_alter( &$css ) {
 
@@ -13,6 +14,40 @@ function bellcom_css_alter( &$css ) {
     // Forum
     unset($css[drupal_get_path('module', 'forum') . '/forum.css']);
 
+    // Mega menu
+    unset($css[drupal_get_path('module', 'tb_megamenu') . '/css/bootstrap.css']);
+    unset($css[drupal_get_path('module', 'tb_megamenu') . '/css/base.css']);
+    unset($css[drupal_get_path('module', 'tb_megamenu') . '/fonts/font-awesome/css/font-awesome.css']);
+
+}
+
+
+
+
+/**
+ * Implementation of hook_preprocess_page().
+ */
+function bellcom_process_page(&$variables) {
+
+
+}
+
+
+
+/**
+ * Overrides theme_menu_tree().
+ */
+function bellcom_menu_tree__main_menu(&$variables) {
+    return $variables['tree'];
+}
+
+
+
+/**
+ * Overrides theme_menu_tree().
+ */
+function bellcom_menu_tree__user_menu(&$variables) {
+    return $variables['tree'];
 }
 
 /**
@@ -25,15 +60,15 @@ function bellcom_menu_tree__menu_header_top_menu(&$variables) {
 
 function bellcom_menu_link(array $variables) {
 
-    $element = $variables['element'];
-    $sub_menu = '';
+    $element                                    =   $variables['element'];
+    $sub_menu                                   =   '';
 
     if ($element['#below']) {
         unset($element['#below']['#theme_wrappers']);
-        $sub_menu = '<ul>' . drupal_render($element['#below']) . '</ul>';
+        $sub_menu                                 =   '<ul>' . drupal_render($element['#below']) . '</ul>';
     }
 
-    $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+    $output                                     =   l($element['#title'], $element['#href'], $element['#localized_options']);
 
     if ( ( $element['#href'] == $_GET['q'] || ( $element['#href'] == '<front>' && drupal_is_front_page() ) ) && ( empty( $element['#localized_options']['language'] ) ) ) {
         $element['#attributes']['class'][] = 'active';
